42097100a8ccfa1bf5c218c91f57e0a0592eeef9
Ajhaa <atte.h.1998@gmail.com>
2018-09-23 18:43:53 +0300

    ex 15 done

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,16 @@
 class User < ApplicationRecord
   include RatingAverage
 
-  validates :username, uniqueness: true,
-                       length: { minimum: 3 }
+  has_secure_password
 
-  has_many :ratings
+  validates :username, uniqueness: true,
+                       length: { minimum: 3, maximum: 30 }
+
+  validates :password, length: {minimum: 4},
+                       format: /(\S*[A-Z]\S*\d|\S*\d\S*[A-Z])/
+
+  has_many :ratings, dependent: :destroy
+  has_many :beers, through: :ratings
+  has_many :memberships, dependent: :destroy
+  has_many :beer_clubs, through: :memberships
 end
